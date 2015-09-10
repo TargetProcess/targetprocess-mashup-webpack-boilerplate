352a206293042f8fd52202b3e4289ef006547252
Aliaksei Shytkin <e79eas@gmail.com>
2015-09-10 14:37:53 +0300

    Fix test index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 /*globals mashup*/
-console.log(`Mashup ${mashup.variables.mashupPath.slice(-2, -1).pop()} is running!`);
+console.log(`Mashup ${mashup.variables.mashupPath.split('/').slice(-2, -1).pop()} is running!`);
