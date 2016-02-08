3f859adaf5b193a9fdc067d2e2dc0b0506d52ac6
Aliaksei Shytkin <e79eas@gmail.com>
2016-02-08 07:06:54 +0300

    Add more env, fix typo

--- webpack/createConfig.js
+++ webpack/createConfig.js
@@ -71,6 +71,8 @@ const createConfig = (options_) => {
             useConfig: true
         }),
         new webpack.DefinePlugin({
+            '__DEV__': process.env.NODE_ENV !== 'production',
+            '__PRODUCTION__': process.env.NODE_ENV === 'production',
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV)
             }
